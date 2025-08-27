f175138f9472ca588288b208236fe8e4d3c13464
cagrisezgenn <hasmetcagri.sezgen@erbakan.edu.tr>
2025-08-27 18:00:44 +0300

    Delete sdof_PSA_band_avg_aug.m

--- sdof_PSA_band_avg_aug.m
+++
@@ -1,6 +0,0 @@
-function Sab = sdof_PSA_band_avg_aug(t, ag, T1, zeta, band_fac, Np)
-% Band ortalaması alınmış spektral ivme
-    Tvec = linspace(band_fac(1)*T1, band_fac(2)*T1, Np);
-    Sa   = sdof_PSA_vec_aug_ode(t, ag, Tvec, zeta);
-    Sab  = mean(Sa);
-end
